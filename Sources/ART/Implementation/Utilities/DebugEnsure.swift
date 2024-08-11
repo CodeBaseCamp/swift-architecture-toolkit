60b748afb70aea832be47587ae2f024f4b0e6644
CodeBaseCamp <7971250+CodeBaseCamp@users.noreply.github.com>
2024-08-11 18:30:57 +0200

    DebugEnsure: Use inlinable rather than discouraged inline qualifier.

--- Sources/ART/Implementation/Utilities/DebugEnsure.swift
+++ Sources/ART/Implementation/Utilities/DebugEnsure.swift
@@ -4,8 +4,8 @@
 /// using the given `messageClosure` if the given `conditionClosure` evaluates to `false`.
 ///
 /// - important Has an effect only when compiled in debug mode.
-@inline(__always) public func debugEnsure(_ conditionClosure: @autoclosure () -> Bool,
-                                          _ messageClosure: @autoclosure () -> String) {
+@inlinable public func debugEnsure(_ conditionClosure: @autoclosure () -> Bool,
+                                   _ messageClosure: @autoclosure () -> String) {
   #if DEBUG
     ensure(conditionClosure(), messageClosure())
   #endif
