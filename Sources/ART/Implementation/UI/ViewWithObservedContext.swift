e07e519314e7a94cf53972fd3c8c848d40bbbba7
CodeBaseCamp <7971250+CodeBaseCamp@users.noreply.github.com>
2024-09-13 06:28:32 +0200

    ViewWithObservedContext: Add documentation.

--- Sources/ART/Implementation/UI/ViewWithObservedContext.swift
+++ Sources/ART/Implementation/UI/ViewWithObservedContext.swift
@@ -2,12 +2,15 @@
 
 import SwiftUI
 
+/// View observing a given `ViewContext` and wrapping another view of type `Content` which is recreated using changes of
+/// aforementioned context.
 public struct ViewWithObservedContext<
   Model: Equatable,
   Event: Hashable,
   Coeffects: CoeffectsProtocol,
   Content: View
 >: View {
+  /// Context of this view.
   public typealias Context = ViewContext<Model, Event, Coeffects>
 
   @ObservedObject
@@ -15,6 +18,8 @@ public struct ViewWithObservedContext<
 
   private let content: (Context) -> Content
 
+  /// Initializes with the given `context` and the given `content` closure. Upon changes of the `model` of the given
+  /// `context`, the given `content` closure is invoked.
   public init(
     _ context: Context,
     @ViewBuilder content: @escaping (Context) -> Content
